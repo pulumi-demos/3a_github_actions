7c7f4d90527683b8b74bc426f118458ccc461d33
Neo <neo@pulumi.com>
2025-10-21 15:24:15 +0000

    preview changes - neo-preview-1761060255279

--- index.ts
+++ index.ts
@@ -3,7 +3,11 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const bucket = new aws.s3.Bucket("my-bucket", {
+    tags: {
+        owner: "vazudew@gmail.com",
+    },
+});
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
